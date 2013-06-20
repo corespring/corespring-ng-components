ac73bf851fb5562ca9eaf80477e1963d10b0c554
edeustace <ed.eustace@gmail.com>
2013-06-20 11:42:06 +0200

    initial Grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+
+  var config = {
+    pkg: grunt.file.readJSON('package.json'),
+    clean: [ 'build'],
+    uglify: {
+      my_target: {
+        files: {
+          'build/<%= pkg.name %>.min.js': ['build/<%= pkg.name %>.js']
+        }
+      }
+    },
+    coffee: {
+     compileJoined: {
+      options: {
+        join: true
+      },
+      files: {
+        'build/<%= pkg.name %>.js': 'modules/**/src/*.coffee'
+      }
+    },
+  }
+};
+
+grunt.initConfig(config);
+grunt.loadNpmTasks('grunt-contrib-coffee');
+grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-jasmine');
+grunt.loadNpmTasks('grunt-shell');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.registerTask('default', ['clean', 'coffee', 'uglify:my_target']);
+grunt.registerTask('test', [ 'jshint', 'shell:prepareTests', 'jasmine']);
+
+};
